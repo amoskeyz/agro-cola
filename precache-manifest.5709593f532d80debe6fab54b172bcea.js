7fa53a1b75baa249c3f7f69d81015e972a826632
amoskeyz <inspiron.amos@gmail.com>
2020-05-23 08:20:56 +0100

    Updates

---
+++ precache-manifest.5709593f532d80debe6fab54b172bcea.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04acc904a3da3a698c1c2c547f981d7",
+    "url": "/agro-cola/index.html"
+  },
+  {
+    "revision": "11fb133b55e7fdb054c5",
+    "url": "/agro-cola/static/css/main.aaf74504.chunk.css"
+  },
+  {
+    "revision": "b4d8045d93916ed520a3",
+    "url": "/agro-cola/static/js/2.d8af25e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/agro-cola/static/js/2.d8af25e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fb133b55e7fdb054c5",
+    "url": "/agro-cola/static/js/main.f41d7d59.chunk.js"
+  },
+  {
+    "revision": "b2c6622f5a8e7aa40a72",
+    "url": "/agro-cola/static/js/runtime-main.ef4a2eb0.js"
+  },
+  {
+    "revision": "1dcfe51c8a1ba125e09396cec61daca0",
+    "url": "/agro-cola/static/media/4.1dcfe51c.svg"
+  },
+  {
+    "revision": "5063ebce392d519fd4221b1efed5d688",
+    "url": "/agro-cola/static/media/R1.5063ebce.png"
+  },
+  {
+    "revision": "6014b74b48a4d50457edc2051ff427f7",
+    "url": "/agro-cola/static/media/R2.6014b74b.png"
+  },
+  {
+    "revision": "39e12e73a3989da851dfbe45a07c8fd8",
+    "url": "/agro-cola/static/media/R3.39e12e73.png"
+  },
+  {
+    "revision": "97b75e8458285fa9240601d89e529e77",
+    "url": "/agro-cola/static/media/R4.97b75e84.png"
+  },
+  {
+    "revision": "0d88f43324eddb1d43313b437179a0fc",
+    "url": "/agro-cola/static/media/R5.0d88f433.png"
+  },
+  {
+    "revision": "d8eabc28b178212ac1a3a9cf23ee5c3a",
+    "url": "/agro-cola/static/media/R6.d8eabc28.png"
+  },
+  {
+    "revision": "4574ac36ab0e2cc8263c14faadc3aee0",
+    "url": "/agro-cola/static/media/R7.4574ac36.png"
+  },
+  {
+    "revision": "39d46a0517d97d90e206beafd5e450bf",
+    "url": "/agro-cola/static/media/Rec.39d46a05.svg"
+  },
+  {
+    "revision": "3d25cb937eda2d76980c92305ebdd0b7",
+    "url": "/agro-cola/static/media/Rect2.3d25cb93.svg"
+  },
+  {
+    "revision": "daf07ce3207f238df12b54952bd4f8a7",
+    "url": "/agro-cola/static/media/Rect3.daf07ce3.svg"
+  },
+  {
+    "revision": "4348d10bac89ddeee476f9c52e3fb7d3",
+    "url": "/agro-cola/static/media/Rect4.4348d10b.svg"
+  },
+  {
+    "revision": "9db5d5f24ca0b2de1da802010a519bbf",
+    "url": "/agro-cola/static/media/Screenshot.9db5d5f2.jpg"
+  },
+  {
+    "revision": "ac2066bdc5e4f3a9d18876675de5f1c1",
+    "url": "/agro-cola/static/media/person1.ac2066bd.png"
+  },
+  {
+    "revision": "497e78e4af96176b2e9fc811e8db0fd5",
+    "url": "/agro-cola/static/media/picture-frame.497e78e4.jpg"
+  },
+  {
+    "revision": "ed4183dc55d4c5a921b81bec520782b6",
+    "url": "/agro-cola/static/media/secaj.ed4183dc.png"
+  }
+]);
